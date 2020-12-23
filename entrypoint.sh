b2fb5bdbd4a8eeb87f85a00bda38f5143929f5a1
BrayanB24 <babermudezb@correo.udistrital.edu.co>
2020-12-22 23:06:50 -0500

    devops: modificando entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,7 @@ set -e
 set -u
 set -o pipefail
 
-if [ -n "${PARAMETER_STORE:-}" ]; then
-  export EVALUACIONES_MONGO_CRUD_USER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/evaluaciones_mongo_crud/db/username --output text --query Parameter.Value)"
-  export EVALUACIONES_MONGO_CRUD_PASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/evaluaciones_mongo_crud/db/password --output text --query Parameter.Value)"
-fi
+export EVALUACIONES_MONGO_CRUD_USER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/evaluaciones_mongo_crud/db/username --output text --query Parameter.Value)"
+export EVALUACIONES_MONGO_CRUD_PASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/evaluaciones_mongo_crud/db/password --output text --query Parameter.Value)"
 
 exec node dist/main
