1958d959a045f1ac80c6d352baa2c8133dc9fb97
BrayanB24 <babermudezb@correo.udistrital.edu.co>
2020-12-22 21:22:02 -0500

    devops: agregando secretos AWS

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ set -e
 set -u
 set -o pipefail
 
-#exec aws --version
+if [ -n "${PARAMETER_STORE:-}" ]; then
+  export EVALUACIONES_MONGO_CRUD_USER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/evaluaciones_mongo_crud/db/username --output text --query Parameter.Value)"
+  export EVALUACIONES_MONGO_CRUD_PASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/evaluaciones_mongo_crud/db/password --output text --query Parameter.Value)"
+fi
 
 exec node dist/main
